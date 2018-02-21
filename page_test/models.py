af5fab58953b526b8b15496a650ae473b61b79cf
derek_520 <18700862394@139.com>
2018-02-21 19:27:50 +0800

    显示全部省市

--- page_test/models.py
+++ page_test/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Areasinfo(models.Model):
+    atitle = models.CharField(max_length=50)
+    aParent = models.ForeignKey('self',null=True,blank=False) # 自关联属性
+
+    class Meta:
+        db_table = 'areas'
--- page_test/views.py
+++ page_test/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponse
+
 
 # Create your views here.
+from .models import Areasinfo
+
+def areas(request):
+    '''地市'''
+    area = Areasinfo.objects.filter(aParent_id__isnull=True)
+
+    return render(request,'page_test/areas.html',{'area': area})
